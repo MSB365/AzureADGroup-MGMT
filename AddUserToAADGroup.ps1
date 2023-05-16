3369f352bb8229e73c7919f44dbdfa7a95bbe8bf
Drago Petrovic <drago@msb365.net>
2023-05-16 13:21:03 +0200

    Create AddUserToAADGroup.ps1

---
+++ AddUserToAADGroup.ps1
@@ -0,0 +1,389 @@
+#region Description
+<#     
+       .NOTES
+       ==============================================================================
+       Created on:         2023/05/16 
+       Created by:         Drago Petrovic
+       Organization:       MSB365.blog
+       Filename:           AddUserToAADGroup.ps1
+       Current version:    V1.0     
+       Find us on:
+             * Website:         https://www.msb365.blog
+             * Technet:         https://social.technet.microsoft.com/Profile/MSB365
+             * LinkedIn:        https://www.linkedin.com/in/drago-petrovic/
+             * MVP Profile:     https://mvp.microsoft.com/de-de/PublicProfile/5003446
+       ==============================================================================
+       .DESCRIPTION
+       Adding Users to Azure Active Directory Group using CSV.            
+       
+       .NOTES
+       You need to connect to the AzureAD PowerShell Module (Connect-AzureAD)
+       
+	   .EXAMPLE
+       .\AddUserToAADGroup.ps1 
+             
+       .COPYRIGHT
+       Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+       to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+       and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+       The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+       WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+       ===========================================================================
+       .CHANGE LOG
+             V1.00, 2023/05/16 - DrPe - Initial version
+             
+			 
+--- keep it simple, but significant ---
+--- by MSB365 Blog ---
+#>
+#endregion
+#########################################################################################################
+[cmdletbinding()]
+param(
+[switch]$accepteula,
+[switch]$v)
+
+###############################################################################
+#Script Name variable
+$Scriptname = "Add User to Azure AD Group"
+$RKEY = "MSB365_AddUserToAADGroup"
+###############################################################################
+
+[void][System.Reflection.Assembly]::Load('System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a')
+[void][System.Reflection.Assembly]::Load('System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
+
+function ShowEULAPopup($mode)
+{
+    $EULA = New-Object -TypeName System.Windows.Forms.Form
+    $richTextBox1 = New-Object System.Windows.Forms.RichTextBox
+    $btnAcknowledge = New-Object System.Windows.Forms.Button
+    $btnCancel = New-Object System.Windows.Forms.Button
+
+    $EULA.SuspendLayout()
+    $EULA.Name = "MIT"
+    $EULA.Text = "$Scriptname - License Agreement"
+
+    $richTextBox1.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+    $richTextBox1.Location = New-Object System.Drawing.Point(12,12)
+    $richTextBox1.Name = "richTextBox1"
+    $richTextBox1.ScrollBars = [System.Windows.Forms.RichTextBoxScrollBars]::Vertical
+    $richTextBox1.Size = New-Object System.Drawing.Size(776, 397)
+    $richTextBox1.TabIndex = 0
+    $richTextBox1.ReadOnly=$True
+    $richTextBox1.Add_LinkClicked({Start-Process -FilePath $_.LinkText})
+    $richTextBox1.Rtf = @"
+{\rtf1\ansi\ansicpg1252\deff0\nouicompat{\fonttbl{\f0\fswiss\fprq2\fcharset0 Segoe UI;}{\f1\fnil\fcharset0 Calibri;}{\f2\fnil\fcharset0 Microsoft Sans Serif;}}
+{\colortbl ;\red0\green0\blue255;}
+{\*\generator Riched20 10.0.19041}{\*\mmathPr\mdispDef1\mwrapIndent1440 }\viewkind4\uc1
+\pard\widctlpar\f0\fs19\lang1033 MSB365 SOFTWARE MIT LICENSE\par
+Copyright (c) 2023 Drago Petrovic\par
+$Scriptname \par
+\par
+{\pict{\*\picprop}\wmetafile8\picw26\pich26\picwgoal32000\pichgoal15
+0100090000035000000000002700000000000400000003010800050000000b0200000000050000
+000c0202000200030000001e000400000007010400040000000701040027000000410b2000cc00
+010001000000000001000100000000002800000001000000010000000100010000000000000000
+000000000000000000000000000000000000000000ffffff00000000ff040000002701ffff0300
+00000000
+}These license terms are an agreement between you and MSB365 (or one of its affiliates). IF YOU COMPLY WITH THESE LICENSE TERMS, YOU HAVE THE RIGHTS BELOW. BY USING THE SOFTWARE, YOU ACCEPT THESE TERMS.\par
+\par
+MIT License\par
+{\pict{\*\picprop}\wmetafile8\picw26\pich26\picwgoal32000\pichgoal15
+0100090000035000000000002700000000000400000003010800050000000b0200000000050000
+000c0202000200030000001e000400000007010400040000000701040027000000410b2000cc00
+010001000000000001000100000000002800000001000000010000000100010000000000000000
+000000000000000000000000000000000000000000ffffff00000000ff040000002701ffff0300
+00000000
+}\par
+\pard
+{\pntext\f0 1.\tab}{\*\pn\pnlvlbody\pnf0\pnindent0\pnstart1\pndec{\pntxta.}}
+\fi-360\li360 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \par
+\pard\widctlpar\par
+\pard\widctlpar\li360 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\par
+\par
+\pard\widctlpar\fi-360\li360 2.\tab THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. \par
+\pard\widctlpar\par
+\pard\widctlpar\fi-360\li360 3.\tab IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. \par
+\pard\widctlpar\par
+\pard\widctlpar\fi-360\li360 4.\tab DISCLAIMER OF WARRANTY. THE SOFTWARE IS PROVIDED \ldblquote AS IS,\rdblquote  WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL MSB365 OR ITS LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\par
+\pard\widctlpar\par
+\pard\widctlpar\fi-360\li360\qj 5.\tab LIMITATION ON AND EXCLUSION OF DAMAGES. IF YOU HAVE ANY BASIS FOR RECOVERING DAMAGES DESPITE THE PRECEDING DISCLAIMER OF WARRANTY, YOU CAN RECOVER FROM MICROSOFT AND ITS SUPPLIERS ONLY DIRECT DAMAGES UP TO U.S. $1.00. YOU CANNOT RECOVER ANY OTHER DAMAGES, INCLUDING CONSEQUENTIAL, LOST PROFITS, SPECIAL, INDIRECT, OR INCIDENTAL DAMAGES. This limitation applies to (i) anything related to the Software, services, content (including code) on third party Internet sites, or third party applications; and (ii) claims for breach of contract, warranty, guarantee, or condition; strict liability, negligence, or other tort; or any other claim; in each case to the extent permitted by applicable law. It also applies even if MSB365 knew or should have known about the possibility of the damages. The above limitation or exclusion may not apply to you because your state, province, or country may not allow the exclusion or limitation of incidental, consequential, or other damages.\par
+\pard\widctlpar\par
+\pard\widctlpar\fi-360\li360\qj 6.\tab ENTIRE AGREEMENT. This agreement, and any other terms MSB365 may provide for supplements, updates, or third-party applications, is the entire agreement for the software.\par
+\pard\widctlpar\qj\par
+\pard\widctlpar\fi-360\li360\qj 7.\tab A complete script documentation can be found on the website https://www.msb365.blog.\par
+\pard\widctlpar\par
+\pard\sa200\sl276\slmult1\f1\fs22\lang9\par
+\pard\f2\fs17\lang2057\par
+}
+"@
+    $richTextBox1.BackColor = [System.Drawing.Color]::White
+    $btnAcknowledge.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right
+    $btnAcknowledge.Location = New-Object System.Drawing.Point(544, 415)
+    $btnAcknowledge.Name = "btnAcknowledge";
+    $btnAcknowledge.Size = New-Object System.Drawing.Size(119, 23)
+    $btnAcknowledge.TabIndex = 1
+    $btnAcknowledge.Text = "Accept"
+    $btnAcknowledge.UseVisualStyleBackColor = $True
+    $btnAcknowledge.Add_Click({$EULA.DialogResult=[System.Windows.Forms.DialogResult]::Yes})
+
+    $btnCancel.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right
+    $btnCancel.Location = New-Object System.Drawing.Point(669, 415)
+    $btnCancel.Name = "btnCancel"
+    $btnCancel.Size = New-Object System.Drawing.Size(119, 23)
+    $btnCancel.TabIndex = 2
+    if($mode -ne 0)
+    {
+   $btnCancel.Text = "Close"
+    }
+    else
+    {
+   $btnCancel.Text = "Decline"
+    }
+    $btnCancel.UseVisualStyleBackColor = $True
+    $btnCancel.Add_Click({$EULA.DialogResult=[System.Windows.Forms.DialogResult]::No})
+
+    $EULA.AutoScaleDimensions = New-Object System.Drawing.SizeF(6.0, 13.0)
+    $EULA.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font
+    $EULA.ClientSize = New-Object System.Drawing.Size(800, 450)
+    $EULA.Controls.Add($btnCancel)
+    $EULA.Controls.Add($richTextBox1)
+    if($mode -ne 0)
+    {
+   $EULA.AcceptButton=$btnCancel
+    }
+    else
+    {
+        $EULA.Controls.Add($btnAcknowledge)
+   $EULA.AcceptButton=$btnAcknowledge
+        $EULA.CancelButton=$btnCancel
+    }
+    $EULA.ResumeLayout($false)
+    $EULA.Size = New-Object System.Drawing.Size(800, 650)
+
+    Return ($EULA.ShowDialog())
+}
+
+function ShowEULAIfNeeded($toolName, $mode)
+{
+$eulaRegPath = "HKCU:Software\Microsoft\$RKEY"
+$eulaAccepted = "No"
+$eulaValue = $toolName + " EULA Accepted"
+if(Test-Path $eulaRegPath)
+{
+$eulaRegKey = Get-Item $eulaRegPath
+$eulaAccepted = $eulaRegKey.GetValue($eulaValue, "No")
+}
+else
+{
+$eulaRegKey = New-Item $eulaRegPath
+}
+if($mode -eq 2) # silent accept
+{
+$eulaAccepted = "Yes"
+        $ignore = New-ItemProperty -Path $eulaRegPath -Name $eulaValue -Value $eulaAccepted -PropertyType String -Force
+}
+else
+{
+if($eulaAccepted -eq "No")
+{
+$eulaAccepted = ShowEULAPopup($mode)
+if($eulaAccepted -eq [System.Windows.Forms.DialogResult]::Yes)
+{
+        $eulaAccepted = "Yes"
+        $ignore = New-ItemProperty -Path $eulaRegPath -Name $eulaValue -Value $eulaAccepted -PropertyType String -Force
+}
+}
+}
+return $eulaAccepted
+}
+
+if ($accepteula)
+    {
+         ShowEULAIfNeeded "DS Authentication Scripts:" 2
+         "EULA Accepted"
+    }
+else
+    {
+        $eulaAccepted = ShowEULAIfNeeded "DS Authentication Scripts:" 0
+        if($eulaAccepted -ne "Yes")
+            {
+                "EULA Declined"
+                exit
+            }
+         "EULA Accepted"
+    }
+#########################################################################################################
+###############################################################################
+write-host "  _           __  __ ___ ___   ____  __ ___  " -ForegroundColor Yellow
+write-host " | |__ _  _  |  \/  / __| _ ) |__ / / /| __| " -ForegroundColor Yellow
+write-host " | '_ \ || | | |\/| \__ \ _ \  |_ \/ _ \__ \ " -ForegroundColor Yellow
+write-host " |_.__/\_, | |_|  |_|___/___/ |___/\___/___/ " -ForegroundColor Yellow
+write-host "       |__/                                  " -ForegroundColor Yellow
+Start-Sleep -s 2
+write-host ""                                                                                   
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+###############################################################################
+
+			Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'MSB365.blog'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(75,120)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(150,120)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Please enter the Name of the Azure AD Group:'
+$form.Controls.Add($label)
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+
+$form.Topmost = $true
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $res1 = $textBox.Text
+	$group = $res1
+}
+Start-Sleep -s 2
+Write-Host "The Group: $group is set!" -ForegroundColor Cyan
+Start-Sleep -s 6
+
+
+# Getting CSV Information
+			write-host "Please select and import the CSV File from your device:" -ForegroundColor Cyan
+			Write-Host ""
+			Write-Host ""
+			Start-Sleep -s 4
+			Write-Host "*****************************************************" -ForegroundColor Yellow -BackgroundColor Black
+			Write-Host "NOTE!" -ForegroundColor Yellow -BackgroundColor Black
+			Write-Host "The following information are needed in the CSV file:" -ForegroundColor White -BackgroundColor black
+			Write-Host '"UserPrincipalName","DisplayName"' -ForegroundColor Gray -BackgroundColor Black
+			Write-Host "*****************************************************" -ForegroundColor Yellow -BackgroundColor Black
+			Start-Sleep -s 6
+
+
+
+			$File = New-Object System.Windows.Forms.OpenFileDialog
+			$null = $File.ShowDialog()
+			$FilePath = $File.FileName
+			$users = Import-Csv $FilePath
+			Start-Sleep -s 3
+			Write-Host "The following data is imported from CSV:" -ForegroundColor Cyan
+			$users | ft
+			Start-Sleep -s 3
+			do
+			{
+				$selection = Read-Host "Are the data correct? - Choose between [Y] and [N]"
+				switch ($selection)
+				{
+					'y' {
+						
+					} 'n' {
+						$File = New-Object System.Windows.Forms.OpenFileDialog
+						$null = $File.ShowDialog()
+						$FilePath = $File.FileName
+						$users = Import-Csv $FilePath
+						Start-Sleep -s 3
+						Write-Host "*****************************************************" -ForegroundColor Yellow -BackgroundColor Black
+						Write-Host "The following data is imported from CSV:" -ForegroundColor Cyan
+						$users | ft
+						Start-Sleep -s 3
+					}
+				}
+			}
+			until ($selection -eq "y")
+			
+
+			start-sleep -s 3
+
+
+$GroupObjectID = Get-AzureADGroup -SearchString $group | Select -Property ObjectID
+# Adding Users to the AAD Group
+			write-host "Adding Users to the Group..." -ForegroundColor cyan 
+			foreach ($y in $users){
+	
+    $y2 = Get-AzureADUser -ObjectId $y.userPrincipalName | Select -Property ObjectID
+    $members = Get-AzureADGroupMember -ObjectId $GroupObjectID.ObjectID -All $true
+   
+    if ($y2.ObjectID -in $members.ObjectID) {
+        Write-Host $y.DisplayName'is already in the Group' -ForegroundColor Blue
+		Start-Sleep -s 1
+    }else{
+        Add-AzureADGroupMember -ObjectId $GroupObjectID.ObjectID -RefObjectId $y2.ObjectId -InformationAction SilentlyContinue
+        Write-Host $y.DisplayName'has been added to the Group' -ForegroundColor Green
+		Start-Sleep -s 1
+    }
+   
+}
+			start-sleep -s 3
+			Write-Host "Users successfuly added to the Group $group!" -ForegroundColor Green -BackgroundColor Black
+			Start-Sleep -s 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Name of the group being added
+$group = "_TMP_PhishSim_ITP"
+
+
+#get the object ID from Azure
+$GroupObjectID = Get-AzureADGroup -SearchString $group | Select -Property ObjectID
+
+
+#roll through the File to look up each user and add to the group. 
+foreach ($y in $File){
+	
+    $y2 = Get-AzureADUser -ObjectId $y.userPrincipalName | Select -Property ObjectID
+    $members = Get-AzureADGroupMember -ObjectId $GroupObjectID.ObjectID -All $true
+   
+    if ($y2.ObjectID -in $members.ObjectID) {
+        Write-Host $y.DisplayName'is already in the Group' -ForegroundColor Blue
+		Start-Sleep -s 1
+    }else{
+        Add-AzureADGroupMember -ObjectId $GroupObjectID.ObjectID -RefObjectId $y2.ObjectId -InformationAction SilentlyContinue
+        Write-Host $y.DisplayName'has been added to the Group' -ForegroundColor Green
+		Start-Sleep -s 1
+    }
+   
+}
+
